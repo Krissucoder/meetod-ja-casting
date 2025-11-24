6920ced5adbd3b95fa73180a275dc139a17a833b
Opilane TTHK <opilane@tthk.ee>
2025-11-24 11:42:27 +0200

    ül

---
+++ Ülesanded/Program.cs
@@ -0,0 +1,63 @@
+﻿//using System.Security.Cryptography.X509Certificates;
+
+//namespace Ülesanded
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+            //Console.WriteLine("Tere! mis on sinu nimi?");
+            //string nimi = "";
+            //do
+            //{
+            //    Console.WriteLine("Sinu nimi on:");
+            //    nimi = Console.ReadLine();
+            //} while (nimi == "");
+            //{
+            //    string kasutajasisestus = "";
+            //    while (kasutajasisestus != "rohem pole")
+            //    {
+            //        List<float> kasutajatulud = new List<float>();
+            //        Console.WriteLine("Mis on teie sissetulekud? \n kui rohkem ei ole midagi lisada,siis kirjuta \"rohkem pole\":");
+            //        kasutajasisestus = Console.ReadLine();
+            //        if (kasutajasisestus != "rohkem pole")
+            //        {
+            //            kasutajatulud.Add(float.Parse(kasutajasisestus));
+            //        }
+            //    }
+            //    while (kasutajasisestus != "rohkem pole")
+            //    {
+            //        List<float> kasutajakulud = new List<float>();
+            //        Console.WriteLine("Mis on teie väljaminekud? \n kui rohkem ei ole midagi lisada, siis kirjuta \"rohkem pole\"");
+            //        kasutajasisestus = Console.ReadLine();
+            //        if (kasutajasisestus != "rohkem pole") ;
+            //        {
+            //            kasutajakulud.Add(float.Parse(kasutajasisestus));
+            //        }
+            //    }
+               
+                
+            //}
+      
+
+//        }
+//    }
+//}
+
+//küsi kasutajalt tema nime (tsüklis)
+//küsi kasutajalt tema tulusid, niikaua kuni rohkem pole
+//siis küsi kasutajalt tema kulusid, ka niikaua kuni rohkem pole.
+//hoia andmeid List tüüpi muutujas, andmed ise peavad olEeema komakohaga.
+//tee uus meetod, mis arvutab kokku järjendi sisu
+//hoia kogutulusid ja kogukulusid eraldi muutujates kasutades oma meetodit.
+//arvuta kasutajale tulude ja kulude põhjal tema hetkeseis.
+//Ümarda kasutajale tagastatav arv kahe komakohani. Ja kuva nimeline sõnum
+
+Public static float Arvutus(List<float>kokkuarvutatavokollektsioon)
+{
+    foreach (var arv in kokkuarvutatavokollektsioon)
+    {
+        
+    }
+    return null;
+}
